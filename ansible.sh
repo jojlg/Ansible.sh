7f5758aa014b27bb280ff6111b24b77387b891d3
jojlg <135955870+jojlg@users.noreply.github.com>
2024-11-12 16:26:49 +0100

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,34 +1,36 @@
-# Créer l'utilisateur ansible et définir un mot de passe provisoire
+#Crée un comtpe de service et installe Ansible
+
+# Crée l'utilisateur ansible et définir un mot de passe provisoire
 adduser --gecos "" ansible
 echo "ansible:password" | chpasswd
 
-# Forcer l'utilisateur à changer son mot de passe à la prochaine connexion
+# Force l'utilisateur à changer son mot de passe à la prochaine connexion
 chage -d 0 ansible
 
-# Ajouter l'utilisateur ansible au groupe sudo
+# Ajoute l'utilisateur ansible au groupe sudo
 usermod -aG sudo ansible
 chown -R ansible:ansible /home/ansible/
 
-# Passer à l'utilisateur ansible pour effectuer l'installation et les autres actions
+# Passe à l'utilisateur ansible pour effectuer l'installation et les autres actions
 sudo -u ansible bash << EOF
-# Mettre à jour la liste des paquets
+# Mets à jour la liste des paquets
 sudo apt-get update -y
 
-# Installer Ansible et tree (facultatif)
+# Installe Ansible et tree (facultatif)
 sudo apt-get install ansible -y
 sudo apt install tree -y
 
-# Vérifier la version d'Ansible installée
+# Vérifie la version d'Ansible installée
 ansible --version
 
-# Créer le répertoire .ssh et générer la clé SSH
+# Crée le répertoire .ssh et générer la clé SSH
 mkdir -p /home/ansible/.ssh
 cd /home/ansible/.ssh
 
-# Générer une clé SSH ed25519 (le nom sera 'id_srv-ansible')
+# Génére une clé SSH ed25519 (le nom sera 'id_srv-ansible')
 ssh-keygen -t ed25519 -f id_srv-ansible -N ""
 
-# Ajouter la clé publique au fichier authorized_keys
+# Ajoute la clé publique au fichier authorized_keys
 cat id_srv-ansible.pub >> authorized_keys
 
 EOF
