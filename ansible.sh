4839bc4dd2a70d0fb40be6f7492b5b510d6eeb41
jojlg <135955870+jojlg@users.noreply.github.com>
2024-12-05 14:20:42 +0100

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,39 +1,70 @@
-# Crée l'utilisateur ansible et définir un mot de passe provisoire
-adduser --gecos "" ansible
-echo "ansible:password" | chpasswd
+# Mise à jour les paquets existants
+sudo apt-get update
 
-# Force l'utilisateur à changer son mot de passe à la prochaine connexion
-chage -d 0 ansible
+# Installe les paquets nécessaires pour Docker
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Ajoute l'utilisateur ansible au groupe sudo
-usermod -aG sudo ansible
-chown -R ansible:ansible /home/ansible/
+# Ajoute la clé GPG officielle de Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Configure sudoers pour l'utilisateur ansible
-echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+# Ajoute le dépôt Docker à APT
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-# Passe à l'utilisateur ansible pour effectuer l'installation et les autres actions
-sudo -u ansible bash << EOF
+# Mise à jour la base de données des paquets
+sudo apt-get update
+
+# Installe Docker
+sudo apt-get install -y docker-ce
+
+# Active Docker pour qu'il démarre au démarrage du système
+sudo systemctl enable docker
+
+# Vérifie l'installation de Docker
+sudo systemctl status docker
+
+# Crée l'utilisateur docker et définir un mot de passe provisoire
+adduser --gecos "" docker
+echo "docker:password" | chpasswd
+
+# Ajoute l'utilisateur docker au groupe sudo et au groupe docker
+usermod -aG sudo docker
+usermod -aG docker docker
+chown -R docker:docker /home/docker/
+
+# Configure sudoers pour l'utilisateur docker
+echo "docker ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Passe à l'utilisateur docker pour effectuer l'installation et les autres actions
+sudo -u docker bash << EOF
 # Mets à jour la liste des paquets
 sudo apt-get update -y
 
-# Installe Ansible et tree (facultatif)
-sudo apt-get install ansible -y
-sudo apt install tree -y
+# Installe Python et pip
+sudo apt-get install -y python3 python3-pip
 
-# Vérifie la version d'Ansible installée
-ansible --version
+# Vérifie l'installation de Python
+python3 --version
+pip3 --version
+
+# Installe Docker-Compose
+DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Vérifier l'installation de Docker Compose
+docker-compose --version
 
 # Crée le répertoire .ssh et générer la clé SSH
-mkdir -p /home/ansible/.ssh
-cd /home/ansible/.ssh
+mkdir -p /home/docker/.ssh
+cd /home/docker/.ssh
 
-# Génére une clé SSH ed25519 (le nom sera 'id_srv-ansible')
-ssh-keygen -t ed25519 -f id_srv-ansible -N ""
+# Génére une clé SSH ed25519 (le nom sera 'id_srv-docker')
+ssh-keygen -t ed25519 -f id_srv-docker -N ""
 
 # Ajoute la clé publique au fichier authorized_keys
-cat id_srv-ansible.pub >> authorized_keys
+cat id_srv-docker.pub >> authorized_keys
 
 EOF
 
-echo "Configuration terminée. L'utilisateur 'ansible' est prêt à être utilisé, son mot de passe sera à modifier à la prochaine connexion"
+echo "Installation de Docker, Python et Docker Compose terminée avec succès."
+echo "Configuration SSH pour l'utilisateur 'docker' terminée. L'utilisateur 'docker' est prêt à être utilisé, son mot de passe sera à modifier à la prochaine connexion."
